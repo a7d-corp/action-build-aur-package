d17e36a1e66dc92f6cddbabd5a97a84f7d846641
Simon Weald <glitchcrab-github@simonweald.com>
2021-01-12 20:30:17 +0000

    more changes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM glitchcrab/arch-build-container:latest
 
 USER root
-
 COPY entrypoint.sh /entrypoint.sh
-
-USER notroot
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -o nounset
 set -o pipefail
 
 main() {
   # sanity check required files
   check_requirements
 
+  # prep SSH
+  prepare_ssh
+
   # pick up variables needed to run
   source VARS.env
 
   # get tag of the latest version
-  LATEST_TAG=$(get_latest_version "${REPO}")
+  LATEST_TAG=$(get_latest_version "${UPSTREAM_REPO}")
   check_response "${LATEST_TAG}" LATEST_TAG
 
   # pick up the version of the last package build
@@ -22,7 +24,7 @@ main() {
   compare_versions "${CURRENT_VERSION}" "${LATEST_TAG}"
 
   # get the asset download url
-  ASSET_URL=$(get_asset_url "${REPO}" "${ASSET_FILE}")
+  ASSET_URL=$(get_asset_url "${UPSTREAM_REPO}" "${ASSET_FILE_STUB}")
   check_response "${ASSET_URL}" ASSET_URL
 
   # download the asset file
@@ -40,16 +42,43 @@ main() {
   # move into the AUR checkout
   cd aur_repo
 
-  ## update pkgbuild with sha256sum and version
-  ## drop pkgrel if updating version
+  # update pkgbuild with sha256sum and version
+  sed -i "s/^pkgver.*/pkgver=${LATEST_TAG}/g" PKGBUILD
+  sed -i "s/^sha256sums.*/sha256sums=('${ASSET_SHA}')/g" PKGBUILD
 
-  #namcap pkgbuild
-  #build pkg file
-  #namcap pkg file
-  #install
-  #update .SRCINFO
-  #commit
-  #push
+  # drop pkgrel back to 1
+  sed -i "s/^pkgrel.*/pkgrel=1/g" PKGBUILD
+
+  # check pkgbuild with namcap
+  if ! namcap PKGBUILD ; then
+    err "PKGBUILD failed namcap check"
+  fi
+
+  # build package
+  makepkg
+
+  # check package file with namcap
+  find -name \*pkg.tar.zst -exec namcap {} \;
+
+  # test installing package
+  find -name \*pkg.tar.zst -exec pacman -U {} \;
+
+  # update .SRCINFO
+  makepkg --printsrcinfo > .SRCINFO
+
+  # prepare git config
+  git config --global user.email "${GIT_EMAIL}"
+  git config --global user.name "${GIT_USER}"
+
+  if ! git add PKGBUILD .SRCINFO ; then
+    err "Couldn't add files for committing"
+  fi
+
+  git commit -m "bump to ${LATEST_TAG}"
+
+  if ! git push ; then
+    err "Couldn't push commit to the AUR"
+  fi
 }
 
 # helper functions
@@ -87,12 +116,31 @@ check_requirements() {
   fi
 }
 
+prepare_ssh() {
+  # prepares the container for SSH
+
+  if [ ! -d $HOME/.ssh ] ; then
+    mkdir -m 0700 $HOME/.ssh
+  fi
+
+  # pull down the public key(s) from the AUR servers
+  if ! ssh-keyscan aur.archlinux.org > $HOME/.ssh/known_hosts ; then
+    err "Couldn't get SSH public key from AUR servers"
+  fi
+
+  # write the private SSH key out to disk
+  if [ ! -z "${AUR_SSH_KEY}" ] ; then
+    echo "${AUR_SSH_KEY}" > $HOME/.ssh/ssh_key
+    chmod 0400 $HOME/.ssh/ssh_key
+  fi
+}
+
 check_response() {
   # takes two inputs and calls err() if the variable is empty
   # $1 - variable name (for logging)
   # $2 - variable value (for checking)
 
-  [ ! -z "${2}" ] || err "${1} is an empty var"
+  [ ! -z "${1}" ] || err "${2} is an empty var"
 }
 
 get_latest_version() {
@@ -109,9 +157,9 @@ get_asset_url() {
   # $1 - repo in format 'org/repo'
   # $2 - asset file name stub to match
 
-  if [ ! -z "${GITHUB_TOKEN}" ]; then
+  if [ ! -z "${PERSONAL_ACCESS_TOKEN}" ]; then
     curl --silent \
-      -H "Authorization: token ${GITHUB_TOKEN}"
+      -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}"
       "https://api.github.com/repos/${1}/releases/latest" \
       | jq -r --arg ASSET_FILE "${2}" \
       '.assets[] | select(.name | contains($ASSET_FILE)) | .browser_download_url'
